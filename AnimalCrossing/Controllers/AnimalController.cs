78643d9a6db501987b04bcb254fb66dab9262aad
ssmith1992 <33715458+ssmith1992@users.noreply.github.com>
2019-10-16 12:36:00 +0200

    set up commit

---
+++ AnimalCrossing/Controllers/AnimalController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AnimalCrossing.Data;
+using AnimalCrossing.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AnimalCrossing.Controllers
+{
+    public class AnimalController : Controller
+    {
+        private readonly AnimalCrossingContext _context;
+
+        public AnimalController(AnimalCrossingContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Cat> cats = _context.Cats.ToList();
+
+            return View("ShowCats", cats);
+        }
+
+
+        public string Hello()
+        {
+            return "Well, hello there! We are learning .NET Core now...";
+        }
+
+        public IActionResult MyFirstView()
+        {
+            ViewBag.MyWifeSays = "Go buy groceries, clean up, make dinner";
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Cat c)
+        {
+            if (ModelState.IsValid) {
+                ViewBag.Thanks = c.Name;
+                ViewBag.Cat = c;
+
+                _context.Cats.Add(c);
+                _context.SaveChanges();
+
+                return View("Thanks", c);
+            }
+            return View(c);
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            // Create an edit view
+            // Look up cat object from catId in the database
+            // Show an edit view to the user, displaying the cat object
+            Cat cat = _context.Cats.Find(id);
+
+
+            return View(cat);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Cat c)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Cats.Update(c);
+                _context.SaveChanges();
+                // Save it to the database
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+    }
+}
---
+++ AnimalCrossing/Models/Cat.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimalCrossing.Models
+{
+    public class Cat
+    {
+        // C# properties = java attribute and public get and set.
+        //private int JavaCatId;
+
+        //public int getJavaCatId()
+        //{
+        //    return this.JavaCatId;
+        //}
+        //public void setJavaCatId(int catId)
+        //{
+        //    this.JavaCatId = catId;
+        //}
+
+
+        // If the name is *class_name*Id
+        public int CatId { get; set; }
+
+        
+        [Required(ErrorMessage = "All cats must have a name to be a pet")]
+        public string Name { get; set; }
+
+        // Later, create 1-to-many relationship to Species table
+        //public string Species { get; set; }
+
+        public Gender? Gender { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name="Birth Date")]
+        public DateTime? BirthDate { get; set; }
+
+        public string ProfilePicture { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 10)]
+        public string Description { get; set; }
+
+        // Ratings..Comments, Reviews
+        // 
+        
+        public Cat()
+        {
+        }
+
+
+    }
+
+    public enum Gender { Male, Female, Other };
+}
